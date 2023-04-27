951d1284d41d993391669b1842f15868a054f887
Alliahsam <alliah2@hotmail.fr>
2023-04-27 08:23:10 -0700

    debugging prompt

--- main.c
+++ main.c
@@ -7,6 +7,11 @@
 
 int main(int argc, char **argv)
 {
+	for (int i = 0; i < argc; i++)
+    {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
 	char *cmd;
 	
 	print_prompt();                                                                                                                                                                                                                   
