9775cc558090d6377d90c3793ccf149f089853a4
Alliahsam <alliah2@hotmail.fr>
2023-04-27 07:01:06 -0700

    add new function to main.c

--- main.c
+++ main.c
@@ -42,3 +42,65 @@ int main(int argc, char **argv)
 
 	exit(EXIT_SUCCESS);
 }
+
+/**
+ * read_cmd: function that read command
+ */
+
+char *read_cmd(void)
+{
+    char *line = NULL;
+    size_t bufsize = 0;
+    ssize_t buflen;
+    char *ptr = NULL;
+    size_t ptrlen = 0;
+
+    while ((buflen = getline(&line, &bufsize, stdin)) != -1)
+    {
+        if (!ptr)
+        {
+            ptr = malloc(buflen + 1);
+        }
+        else
+        {
+            char *ptr2 = realloc(ptr, ptrlen + buflen + 1);
+
+            if (ptr2)
+            {
+                ptr = ptr2;
+            }
+            else
+            {
+                free(ptr);
+                ptr = NULL;
+            }
+        }
+
+        if (!ptr)
+        {
+            fprintf(stderr, "error: failed to allocate buffer: %s\n", strerror(errno));
+            free(line);
+            return NULL;
+        }
+
+        strcpy(ptr + ptrlen, line);
+
+        if (line[buflen - 1] == '\n')
+        {
+            if (buflen == 1 || line[buflen - 2] != '\\')
+            {
+                free(line);
+                return ptr;
+            }
+
+            ptr[ptrlen + buflen - 2] = '\0';
+            buflen -= 2;
+        }
+
+        ptrlen += buflen;
+    }
+
+    free(line);
+    return ptr;
+}
+
